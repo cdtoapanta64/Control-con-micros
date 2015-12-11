44727c36cde4c9bba17b1402dc40e6532275a42a
Andr3sS <jandy_2693@hotmail.com>
2015-12-11 11:14:19 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,51 @@
+/*
+ * Prueba_Control_con_Micros.c
+ *
+ * Created: 11/12/2015 10:41:27
+ *  Author: USUARIO
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "lcd.h"
 
+void puertos(void);
+void configtimmers(void);
+void adacelerador(void);
+void adtanque(void);
+void EEPROM_write(unsigned int uiAddress, unsigned char var);
+unsigned char EEPROM_read(unsigned int uiAddress);
+
+int main(void)
+{
+    while(1)
+    {
+        
+    }
+}
+
+void EEPROM_write(unsigned int uiAddress, unsigned char var)
+{
+	/* Wait for completion of previous write */
+	while(EECR & (1<<EEPE));
+	
+	EEAR = uiAddress;
+	EEDR = var;
+	/* Write logical one to EEMPE */
+	EECR |= (1<<EEMPE);
+	/* Start eeprom write by setting EEPE */
+	EECR |= (1<<EEPE);
+}
+
+unsigned char EEPROM_read(unsigned int uiAddress)
+{
+	/* Wait for completion of previous write */
+	while(EECR & (1<<EEPE))
+	;
+	/* Set up address register */
+	EEAR = uiAddress;
+	/* Start eeprom read by writing EERE */
+	EECR |= (1<<EERE);
+	/* Return data from Data Register */
+	return EEDR;
+}
