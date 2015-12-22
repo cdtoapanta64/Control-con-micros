48c97485e012cd69907cb2be3da48eedec1123b6
Daniel Toapanta <cdtoapanta64@hotmail.com>
2015-12-21 23:36:37 -0500

    Update main.c

--- main.c
+++ main.c
@@ -9,37 +9,36 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "lcd.h"
+#include "math.h"
+#include <util/delay.h>
+#include <avr/eeprom.h>
+
 
 int valoracelerador,valortanque,valorfreno,valorluzfreno;
 int valorluzretro,valortemperatura,encendido=0;
 int contadorvelocidad,contadordistancia;
 int velocidad=0,velocidadmaxima=0,distanciatotal=0,distanciarecorrido=0,distanciausuario=0,tiempo;
 int aux,aux1,velocidadm,velocidadMm,distanciam,distanciarecorridom,distanciausuariom;
-
+uint16_t reco=0,reco1=0;
 
 void configinterrupcion(void);
 void iniciomicro(void);
 void configtimmers(void);
 //////Analogo Digital///////
-void adtemperatura(void);
+//void adtemperatura(void);
 void adacelerador(void);
 void adtanque(void);
 void adfreno(void);
-
-
-void EEPROM_write(unsigned int uiAddress, unsigned char var);
-unsigned char EEPROM_read(unsigned int uiAddress);
-
 ////funciones//////
 void gasolina(void);
 void acelerador(void);
-void tempambiente(void);
+//void tempambiente(void);
 void mostrardistanciatotal(void);
 void mostrardistanciarecorrido(void);
 void mostrardistanciausuario(void);
 void freno(void);
 void retro(void);
-///Programa Principal///
+///Program Principal///
 int main(void)
 {
 	iniciomicro();
@@ -48,12 +47,14 @@ int main(void)
 	lcd_clrscr();
 	configtimmers();
 	configinterrupcion();
-	distanciatotal=EEPROM_read(0);
-	distanciausuario=EEPROM_read(1);
+	reco=eeprom_read_word((uint16_t *)10);
+	reco1=eeprom_read_word((uint16_t *)20);
+	distanciatotal=reco;
+	distanciausuario=reco1;	
     while(1)
     {
-		distanciarecorrido=0;
 		
+		encendido=0;
 		while (((PIND & (1<<PD4))==0))
 		{
 			
@@ -67,7 +68,7 @@ int main(void)
 			{
 				OCR2B=0;				
 			}
-			tempambiente();
+			//tempambiente();
 			gasolina();
 			
 			mostrardistanciausuario();
@@ -96,11 +97,10 @@ int main(void)
 	
 	}
 }
-
 void mostrardistanciatotal(void)
 {
 	///////Distancia Total////////////
-	distanciam=distanciatotal*0.1;
+	distanciam=distanciatotal*0.01;
 	lcd_gotoxy(16,0);
 	lcd_puts("Dt");
 	lcd_gotoxy(18,0);
@@ -111,7 +111,7 @@ void mostrardistanciatotal(void)
 void mostrardistanciarecorrido(void)
 {
 	/////Distancia  Recorrido////////////
-	distanciarecorridom=distanciarecorrido*0.1;
+	distanciarecorridom=distanciarecorrido*0.01;
 	lcd_gotoxy(0,1);
 	lcd_puts("Dr");
 	lcd_gotoxy(2,1);
@@ -122,44 +122,42 @@ void mostrardistanciarecorrido(void)
 void mostrardistanciausuario(void)
 {
 	/////Distancia  Usuario////////////
-	distanciausuariom=distanciausuario*0.1;
+	distanciausuariom=distanciausuario*0.01;
 	lcd_gotoxy(8,1);
 	lcd_puts("Du");
 	lcd_gotoxy(10,1);
 	lcd_write_value(distanciausuariom,4);
 	lcd_gotoxy(14,1);
 	lcd_puts("km");
-}
-
-	
-	
-	
+}	
 /////////////////
-void tempambiente(void)
+/*void tempambiente(void)
 {
-	int temp;
+	unsigned temp;
 	adtemperatura();
-	temp=valortemperatura*0.107526;
+	temp=valortemperatura*0.48;
 	lcd_gotoxy(25,0);
 	lcd_puts("Tem:");
 	lcd_gotoxy(29,0);
 	lcd_write_value(temp,2);
-	lcd_gotoxy(31,0);
-	lcd_puts("C");
+	//lcd_gotoxy(31,0);
+	//lcd_puts("C");
 	
 }
+*/
 void gasolina(void)
 {
-	int aux;
+	unsigned aux;
 	adtanque();
-	aux=(valortanque-512)*(0.1953125);
+	aux=0.00113472*pow(valortanque,2)-2.439288*valortanque+1287.16381366;
 	lcd_gotoxy(23,1);
 	lcd_puts("Fuel:");
 	lcd_gotoxy(28,1);
 	lcd_write_value(aux,3);
 	lcd_gotoxy(31,1);
-	lcd_puts("%");
-	PORTD&=~(1<<PD0);
+	//lcd_puts("%");
+	//PORTD&=~(1<<PD0);
+	
 	
 	if(aux<=25)
 	{
@@ -178,7 +176,7 @@ void acelerador(void)
 	int aux;
 	adacelerador();
 	
-	aux=-0.1953125*(valoracelerador-1024);
+	aux=0.21834061*(valoracelerador-493);
 	OCR2B=aux;
 	
 }
@@ -186,10 +184,10 @@ void freno(void)
 {
 	int aux0;
 	adfreno();
-	aux0=0.1953125*(valorfreno-512);
-	if (aux0<80)
+	aux0=-0.1953125*(valorfreno-1024);
+	if (aux0<90)
 	{
-		PORTB|=(1<<PB2);
+		PORTB&=~(1<<PB2);
 		if ((PINB & (1<<PB4))==0)
 		{
 			PORTD|=(1<<PD1);
@@ -201,7 +199,7 @@ void freno(void)
 	} 
 	else
 	{
-		PORTB&=~(1<<PB2);
+		PORTB|=(1<<PB2);
 		PORTD&=~(1<<PD1);
 	}
 }
@@ -209,7 +207,7 @@ void retro(void)
 {
 	if (PINB & (1<<PB6) )
 	{
-		PORTB|=(1<<PB1);
+		PORTB&=~(1<<PB1);
 		if ((PINB & (1<<PB5))==0)
 		{
 			PORTD|=(1<<PD5);
@@ -221,7 +219,7 @@ void retro(void)
 	} 
 	else
 	{
-		PORTB&=~(1<<PB1);
+		PORTB|=(1<<PB1);
 		PORTD&=~(1<<PD5);
 	}
 }
@@ -246,11 +244,12 @@ ISR(TIMER0_COMPB_vect)
 		distanciarecorrido++;
 		distanciausuario++;
 		contadordistancia=0;
-		EEPROM_write(0,distanciatotal);
-		EEPROM_write(1,distanciausuario);
-		
+		reco=distanciatotal;
+		eeprom_write_word((uint16_t *)10,reco);
+		reco1=distanciausuario;
+		eeprom_write_word((uint16_t *)20,reco1);
 	} 
-
+		
 }
 ISR(TIMER1_COMPA_vect)
 {
@@ -269,6 +268,10 @@ ISR(TIMER1_COMPA_vect)
 }
 ISR(INT0_vect)
 {
+	while ((PIND & (1<<PD2))==0)//Si se aplasto espera a que lo suelte
+	{
+		
+	}
 	encendido++;
 	if (encendido==2)
 	{
@@ -278,15 +281,18 @@ ISR(INT0_vect)
 ISR(INT1_vect)
 {
  distanciausuario=0;
+ reco1=0;
+ eeprom_write_word((uint16_t *)20,reco1);
 }
 
 void iniciomicro(void)
 {
 	
-	DDRA=0xff;
-	DIDR0=0b00111111;
+	DDRA=0x00;
+	PORTA=0x00;
+	DIDR0=0xFF;
 	
-	 DDRB=0b00000110;
+	DDRB=0b00000110;
 	PORTB=0b11111000;
 	
 	DDRC=0xFF;
@@ -332,17 +338,17 @@ void configtimmers(void)
 	OCR2B=0;
 	ASSR=0b00000000;
 }
-void adtemperatura(void)
+/*void adtemperatura(void)
 {
-	ADMUX=0B10000101;
-	ADCSRA=0B11000011;
+	ADMUX=0b01000011;
+	ADCSRA=0b11000011;
 	ADCSRA |= (1<<ADSC);
 	// wait until conversion complete ADSC=0 -> Complete
 	while (ADCSRA & (1<<ADSC));
 	// Get ADC the Result
 	valortemperatura= ADCW;
 }
-
+*/
 void adfreno(void)
 {
 	ADMUX=0b01000010;
@@ -355,8 +361,8 @@ void adfreno(void)
 }
 void adacelerador(void)
 {
-	ADMUX=0B01000000;
-	ADCSRA=0B11000011;
+	ADMUX=0b01000000;
+	ADCSRA=0b11000011;
 	ADCSRA |= (1<<ADSC);
 	// wait until conversion complete ADSC=0 -> Complete
 	while (ADCSRA & (1<<ADSC));
@@ -365,8 +371,8 @@ void adacelerador(void)
 }
 void adtanque(void)
 {
-	ADMUX=0B01000001;
-	ADCSRA=0B11000011;
+	ADMUX=0b01000001;
+	ADCSRA=0b11000011;
 	ADCSRA |= (1<<ADSC);
 	// wait until conversion complete ADSC=0 -> Complete
 	while (ADCSRA & (1<<ADSC));
@@ -375,28 +381,3 @@ void adtanque(void)
 	
 }
 
-void EEPROM_write(unsigned int uiAddress, unsigned char var)
-{
-	/* Wait for completion of previous write */
-	while(EECR & (1<<EEPE));
-	
-	EEAR = uiAddress;
-	EEDR = var;
-	/* Write logical one to EEMPE */
-	EECR |= (1<<EEMPE);
-	/* Start eeprom write by setting EEPE */
-	EECR |= (1<<EEPE);
-}
-
-unsigned char EEPROM_read(unsigned int uiAddress)
-{
-	/* Wait for completion of previous write */
-	while(EECR & (1<<EEPE))
-	;
-	/* Set up address register */
-	EEAR = uiAddress;
-	/* Start eeprom read by writing EERE */
-	EECR |= (1<<EERE);
-	/* Return data from Data Register */
-	return EEDR;
-}
